57b8dfda3949caebd4778ac0c68c5eb10023f4d0
inan yigit <inanygt@hotmail.com>
2022-07-31 11:28:02 +0200

    passed 6 test sumAll project

--- sumAll.js
+++ sumAll.js
@@ -1,25 +1,24 @@
 const sumAll = function(num1, num2) {
    let totalSum = 0;
-   smallerValue = Math.min(num1,num2) 
+   let error = "ERROR"
+   smallerValue = Math.min(num1,num2); 
    biggerValue = Math.max(num1,num2);
-   for (let i = smallerValue; i <= biggerValue; i++) {
+   if ((smallerValue < 0 || biggerValue < 0) || 
+   (!Number.isInteger(num1) || (!Number.isInteger(num2))) ) 
+   {
+      return error;
+   } else {
+      for (let i = smallerValue; i <= biggerValue; i++) {
       totalSum = totalSum + i
 
    }
+   }
+   
    return totalSum
 };
 
 // Do not edit below this line
 module.exports = sumAll;
-
-
-
-// 1. create a variable that will hold the sum
-// 2. start with an initialValue / the lowest number
-// 3. Assign the highest number
-// 4. sort the number from low to high 
-// 5. Start adding the next number with the sum untill you reath the highest number
-// 6. return the sum 
 
 
 
